b2121eeab23556e84b3f3c6b844e57b9e70f67c3
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-07-01 10:50:26 -0400

    Create Project

---
+++ src/main/java/com/devYunes/ezpizza/EzpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.devYunes.ezpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devYunes/ezpizza/EzpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devYunes.ezpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
